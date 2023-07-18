0e276f8f14971ac0f8b3feb5afc8ee0c11bdb6fb
Agus Client <iAgusClient@hotmail.com>
2023-07-17 23:31:22 -0300

    api auth y model de user

---
+++ AuthenticationAPI/Controllers/DatabaseConnector.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationAPI.Controllers
+{
+    public class DatabaseConnector
+    {
+        public string dbip;
+        public string dbUser;
+        public string dbPassword;
+        public string dbDatabaseName;
+
+        public MySqlConnection Connection;
+        public MySqlCommand Command;
+        public MySqlDataReader Reader;
+
+        public DatabaseConnector()
+        {
+            this.dbip = "localhost";
+            this.dbUser = "root";
+            this.dbPassword = "zackquack";
+            this.dbDatabaseName = "quickcarry";
+
+            this.Connection = new MySqlConnection(
+                $"server={this.dbip};" +
+                $"user={this.dbUser};" +
+                $"password={this.dbPassword};" +
+                $"database={this.dbDatabaseName};");
+
+            this.Connection.Open();
+            this.Command = new MySqlCommand();
+            this.Command.Connection = this.Connection;
+        }
+    }
+}
