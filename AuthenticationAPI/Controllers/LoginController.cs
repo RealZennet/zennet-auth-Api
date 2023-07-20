07e5def6446d0c9b5915d2c3fd8986b4392d2e86
Damian Suffo <suffodamian02@gmail.com>
2023-07-20 19:06:02 -0300

    clase token generator y  modelo de login request

---
+++ AuthenticationAPI/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using AuthenticationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+
+namespace AuthenticationAPI.Controllers
+{
+
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("api/login/test")]
+        public IHttpActionResult test()
+        {
+            return Ok(true);
+        }
+
+    }
+}
