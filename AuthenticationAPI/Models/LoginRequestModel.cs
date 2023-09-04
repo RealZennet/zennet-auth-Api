f20fd32ff22587d49291efe9ad8257221c3cd99a
Damian Suffo <suffodamian02@gmail.com>
2023-09-04 00:29:32 -0300

    login request model

--- AuthenticationAPI/Models/LoginRequestModel.cs
+++ AuthenticationAPI/Models/LoginRequestModel.cs
@@ -1,4 +1,5 @@
 ﻿using AuthenticationAPI.Controllers;
+using MD5Hash;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -13,10 +14,52 @@ namespace AuthenticationAPI.Models
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
-        [Required]
-        public string userRole { get; set; }
 
-    
+
+        public Dictionary<string, string> LoginRequest()
+        {
+            Dictionary<string, string> resultado = new Dictionary<string, string>();
+
+            
+            this.Command.CommandText = $"SELECT username, pass FROM operario WHERE username = '{this.UserName}'";
+            this.Reader = this.Command.ExecuteReader();
+
+            if (this.Reader.HasRows)
+            {
+                this.Reader.Read();
+                string dbPassword = this.Reader["pass"].ToString();
+
+                
+                if (Hash.Content(this.Password) == dbPassword)
+                {
+                    resultado.Add("resultado", "OK");
+                    resultado.Add("tipo", "operario"); 
+                    return resultado;
+                }
+            }
+
+            
+            this.Command.CommandText = $"SELECT username, pass FROM camionero WHERE username = '{this.UserName}'";
+            this.Reader = this.Command.ExecuteReader();
+
+            if (this.Reader.HasRows)
+            {
+                this.Reader.Read();
+                string dbPassword = this.Reader["pass"].ToString();
+
+                
+                if (Hash.Content(this.Password) == dbPassword)
+                {
+                    resultado.Add("resultado", "OK");
+                    resultado.Add("tipo", "camionero"); 
+                    return resultado;
+                }
+            }
+
+            // Si no se encontró en ninguna de las tablas o la contraseña no coincide, indica un error.
+            resultado.Add("resultado", "Error");
+
+            return resultado;
+        }
     }
-
 }
